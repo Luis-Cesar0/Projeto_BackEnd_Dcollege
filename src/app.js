0fbab738910496358fdd9b9f0cb7754b936d5552
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-08-07 13:52:13 -0300

    Implementar busca por ID na rota e atualização de informações

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ const app = express()
 app.use(express.json())
 
 //rotas
+app.get('/buscaId/:id',(req,res) => {
+    let id = req.params.id
+    let obj = objTeste.find((obj) => obj.id == id)
+    if(obj){
+        res.send({message: responses.success, obj})
+    }else{
+        res.send({message: responses.badRequest})
+    }
+})
 
 // rota padrão para testar as configurações 
 app.get('/',(req,res) => {
