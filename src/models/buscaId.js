c53a8a1af3718bdcb1a5337001ce4ef724144f0a
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-08-07 13:45:38 -0300

    criaçãoda função buscar por id

--- src/models/buscaId.js
+++ src/models/buscaId.js
@@ -1,42 +1,12 @@
-// Criar endpoint para obter informações do usuário pelo ID
+// Criar uma funcao generica endpoint para obter informações do usuário, categoria e produto pelo ID
+import resposta from "../responses"
 
+export default function buscaId(tabela,id) {
 
-// Criar endpoint para obter informações do usuário pelo ID
-Usuario.findByPk(1)
-  .then(usuario => {
-    if (usuario) {
-      console.log('Usuário encontrado:', usuario);
-    } else {
-      console.log('Usuário não encontrado');
+    let obj = tabela.find((obj) => obj.id == id)
+    if(obj){
+        return {message: resposta.success, obj}
+    }else{
+        return {message: resposta.badRequest}
     }
-  })
-  .catch(err => {
-    console.error('Erro ao buscar usuário:', err);
-  });
-
-
- // Criar endpoint para obter informações da categoria pelo ID
-Categoria.findByPk(1)
-  .then(categoria => {
-    if (categoria) {
-      console.log('Categoria encontrada:', categoria);
-    } else {
-      console.log('Categoria não encontrada');
-    }
-  })
-  .catch(err => {
-    console.error('Erro ao buscar categoria:', err);
-  });
-
- //Criar endpoint para obter informações do produto pelo ID
-Produto.findByPk(1)
-  .then(produto => {
-    if (produto) {
-      console.log('Produto encontrado:', produto);
-    } else {
-      console.log('Produto não encontrado');
-    }
-  })
-  .catch(err => {
-    console.error('Erro ao buscar produto:', err);
-  });
+}
