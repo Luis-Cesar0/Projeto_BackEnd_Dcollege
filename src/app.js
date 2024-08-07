bbe2d225a5405d713f12e0c199e588ba190f7c96
Luis-Cesar0 <luiscesarn1@gmail.com>
2024-08-07 16:25:58 -0300

    mudadndo nome do arquivo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const usuarioRoutes = require('./func/getUser')
+const usuarioRoutes = require('./controllers/getUser')
 
 app.use(express.json())
 
