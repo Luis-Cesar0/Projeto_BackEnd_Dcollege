60418db076e04f6de45d206690656ed9fa4ff8d5
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-08-07 14:59:12 -0300

    Implementar busca por ID nas rotas de usuários, produtos e categorias

--- src/app.js
+++ src/app.js
@@ -3,36 +3,37 @@ import objTeste from './objTeste.js'
 import Usuario from './models/tabelaUsuarios.js'
 import Produtos from './models/tabelaProdutos.js'
 import Categoria from './models/tabelaCategoria.js'
-import buscaId from './models/buscaId.js'
+import buscaId from './config/buscaId.js'
 import responses from './responses.js'
 const app = express()
 
 app.use(express.json())
 
-//rota para buscar por id usuario
-app.get('/usuarios/:id', (req, res) => {
-  let id = req.params.id
-  let obj = buscaId(Usuario,id)
-  res.send(obj)
-})
+// //rota para buscar por id usuario
+// app.get('/usuarios/:id', (req, res) => {
+//   let id = req.params.id
+//   let obj = buscaId(Usuario,id)
+//   res.send(obj)
+// })
 
-//rota para buscar por id produto
-app.get('/produtos/:id', (req, res) => {
-  let id = req.params.id
-  let obj = buscaId(Produtos,id)
-  res.send(obj)
-})
+// //rota para buscar por id produto
+// app.get('/produtos/:id', (req, res) => {
+//   let id = req.params.id
+//   let obj = buscaId(Produtos,id)
+//   res.send(obj)
+// })
 
-// Criar endpoint para obter informações da categoria pelo ID
-app.get('/categorias/:id', (req, res) => {
-  let id = req.params.id
-  let obj = buscaId(Categoria,id)
-  res.send(obj)
-})
+// // Criar endpoint para obter informações da categoria pelo ID
+// app.get('/categorias/:id', (req, res) => {
+//   let id = req.params.id
+//   let obj = buscaId(Categoria,id)
+//   res.send(obj)
+// })
 
 // rota padrão para testar as configurações 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/',(req,res) => {
+  res.send('deu certo')
+    
 })
 
 // exportatnado a aplicação Express
