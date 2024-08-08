856c2f92f9a4c9378e887cf6a943d182eb401782
Luis-Cesar0 <luiscesarn1@gmail.com>
2024-08-08 14:43:01 -0300

    excluindo importe

--- src/controllers/contollerUsuarios.js
+++ src/controllers/contollerUsuarios.js
@@ -1,11 +1,10 @@
 const express=require('express');
 const userservice = require('../services/serviceUsario')
-const tabelaUsuarios= require("../models/tabelaUsuarios")
-const tabelaProdutos = require('../models/tabelaProdutos')
+
 
 
 const getUserId =(req,res) =>{
-    userservice(req,res,tabelaUsuarios)
+    userservice(req,res)
 }
 
 module.exports= getUserId
