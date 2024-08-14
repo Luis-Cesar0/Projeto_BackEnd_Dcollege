c946bebcf778a5d66d3b68f9ac67b70aae789602
Luis-Cesar0 <luiscesarn1@gmail.com>
2024-08-14 15:16:15 -0300

    criando ambiente de teste

--- test/home.test.js
+++ test/home.test.js
@@ -1,6 +1,17 @@
 const app = require('../src/app');
 const request = require('supertest');
 
+let server
+
+beforeAll(async () =>{
+    server = await app.listen(4000)
+})
+afterAll(async () =>{
+    server.close()
+})
+
+
+
 test('deve retornar a mensagem de boas-vindas', async () => {
     const response = await request(app).get('/');
 
